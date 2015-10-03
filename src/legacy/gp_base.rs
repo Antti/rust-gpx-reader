e3b254fbe7f2b7af6aeb6fb9fc5f7b4345083fdf
Andrii Dmytrenko <refresh.xss@gmail.com>
2015-10-03 22:16:22 +0100

    Make IoReader trait instead of struct.

--- src/legacy/gp_base.rs
+++ src/legacy/gp_base.rs
@@ -1,14 +1,13 @@
 use super::io_reader::IoReader;
 use super::super::Result;
-use std::io;
 
-pub struct GPFile <T> where T: io::Read {
-    io: IoReader<T>
+pub struct GPFile <T> where T: IoReader {
+    io: T
 }
 
-impl <T> GPFile<T> where T: io::Read {
+impl <T> GPFile<T> where T: IoReader {
     pub fn new(data: T) -> Self {
-        GPFile { io: IoReader::new(data) }
+        GPFile { io: data }
     }
 
     pub fn read_version(&mut self) -> Result<String> {
@@ -20,7 +19,6 @@ impl <T> GPFile<T> where T: io::Read {
 #[cfg(test)]
 mod tests {
     use super::GPFile;
-    use byteorder::{ReadBytesExt, BigEndian, LittleEndian};
     use std::fs::File;
     use std::path::Path;
 
--- src/legacy/io_reader.rs
+++ src/legacy/io_reader.rs
@@ -6,74 +6,66 @@ use encoding::codec::singlebyte::SingleByteEncoding;
 use super::super::Result;
 use super::super::error::Error;
 
-const GPEncoding: &'static SingleByteEncoding  = ::encoding::all::WINDOWS_1251;
+const GPENCODING: &'static SingleByteEncoding  = ::encoding::all::WINDOWS_1251;
 
-pub struct IoReader<T> where T: Read {
-    data: T
-}
-
-impl <T> IoReader <T> where T: Read {
-    pub fn new(data: T) -> Self {
-        IoReader { data: data }
-    }
-
-    pub fn skip(&mut self, n_bytes: i64) -> Result<()> {
+pub trait IoReader: Read {
+    fn skip(&mut self, n_bytes: i64) -> Result<()> {
         for _ in (0..n_bytes) {
             try!(self.read_byte());
         }
         Ok(())
     }
 
-    pub fn read_byte(&mut self) -> Result<u8> {
+    fn read_byte(&mut self) -> Result<u8> {
         let buf = &mut [0u8];
-        try!(self.data.read(buf));
+        try!(self.read(buf));
         Ok(buf[0])
     }
 
-    pub fn read_signed_byte(&mut self) -> Result<i8> {
+    fn read_signed_byte(&mut self) -> Result<i8> {
         let buf = &mut [0u8];
-        try!(self.data.read(buf));
+        try!(self.read(buf));
         Ok(buf[0] as i8)
     }
 
-    pub fn read_bytes(&mut self, n_bytes: usize) -> Result<Vec<u8>> {
+    fn read_bytes(&mut self, n_bytes: usize) -> Result<Vec<u8>> {
         let mut buf = Vec::with_capacity(n_bytes);
-        try!(self.data.read(&mut buf));
+        try!(self.read(&mut buf));
         Ok(buf)
     }
 
-    pub fn read_bool(&mut self) -> Result<bool> {
+    fn read_bool(&mut self) -> Result<bool> {
         self.read_byte().map(|byte| byte > 0)
     }
 
     // Read 2 little-endian bytes as a short integer.
-    pub fn read_short(&mut self) -> Result<i16> {
-        self.data.read_i16::<LittleEndian>().map_err(|err| Error::from(err))
+    fn read_short(&mut self) -> Result<i16> {
+        self.read_i16::<LittleEndian>().map_err(|err| Error::from(err))
     }
 
     // Read 4 little-endian bytes an integer.
-    pub fn read_int(&mut self) -> Result<i32> {
-        self.data.read_i32::<LittleEndian>().map_err(|err| Error::from(err))
+    fn read_int(&mut self) -> Result<i32> {
+        self.read_i32::<LittleEndian>().map_err(|err| Error::from(err))
     }
 
     // Read 4 little-endian bytes as a float.
-    pub fn read_float(&mut self) -> Result<f32> {
-        self.data.read_f32::<LittleEndian>().map_err(|err| Error::from(err))
+    fn read_float(&mut self) -> Result<f32> {
+        self.read_f32::<LittleEndian>().map_err(|err| Error::from(err))
     }
 
     // Read 8 little-endian bytes as a float.
-    pub fn read_double(&mut self) -> Result<f64> {
-        self.data.read_f64::<LittleEndian>().map_err(|err| Error::from(err))
+    fn read_double(&mut self) -> Result<f64> {
+        self.read_f64::<LittleEndian>().map_err(|err| Error::from(err))
     }
 
     // Read length of the string stored in 1 byte and followed by character bytes.
-    pub fn read_byte_sized_string(&mut self, size: usize) -> Result<String> {
+    fn read_byte_sized_string(&mut self, size: usize) -> Result<String> {
         let len = try!(self.read_byte()) as usize;
         self.read_string(size, Some(len))
     }
 
     // Read length of the string stored in 1 integer and followed by character bytes.
-    pub fn read_int_sized_string(&mut self) -> Result<String> {
+    fn read_int_sized_string(&mut self) -> Result<String> {
         let size = try!(self.read_int());
         self.read_string(size as usize, None)
     }
@@ -81,7 +73,7 @@ impl <T> IoReader <T> where T: Read {
     // Read length of the string increased by 1 and stored in 1 integer
     // followed by length of the string in 1 byte and finally followed by
     // character bytes.
-    pub fn read_int_byte_sized_string(&mut self) -> Result<String> {
+    fn read_int_byte_sized_string(&mut self) -> Result<String> {
         let size = try!(self.read_int()) as usize - 1;
         return self.read_byte_sized_string(size)
     }
@@ -101,7 +93,7 @@ impl <T> IoReader <T> where T: Read {
             }
         }
         let mut buf : Vec<u8> = vec![0u8; need_to_read];
-        let read_count = try!(self.data.read(&mut buf));
+        let read_count = try!(self.read(&mut buf));
         if read_count < need_to_read {
             return Err(Error::FormatError(format!("Read {} bytes, expected {}", read_count, need_to_read)));
         }
@@ -109,21 +101,22 @@ impl <T> IoReader <T> where T: Read {
             Some(len) => &buf[0..len],
             None => &buf as &[u8]
         };
-        let s = try!(GPEncoding.decode(truncated_buf, DecoderTrap::Replace));
+        let s = try!(GPENCODING.decode(truncated_buf, DecoderTrap::Replace));
         Ok(s)
     }
 }
 
+impl <T: Read> IoReader for T {}
+
 #[cfg(test)]
 mod tests {
     use super::IoReader;
-    use byteorder::{ReadBytesExt, BigEndian, LittleEndian};
 
     #[test]
     pub fn test_io_reader_read_byte(){
         let data : &[u8] = &[0b11001010, 0b11110000];
         println!("data: {:?}", data);
-        let mut io = IoReader { data: data };
+        let mut io = data;
         let bits : Vec<u8> = (0..2).map(|_|{
             io.read_byte().unwrap()
         }).collect();
@@ -133,7 +126,7 @@ mod tests {
     pub fn test_io_reader_skip(){
         let data : &[u8] = &[0b11001010, 0b11110000];
         println!("data: {:?}", data);
-        let mut io = IoReader { data: data };
+        let mut io = data;
         io.skip(1).unwrap();
         assert_eq!(io.read_byte().unwrap(), data[1]);
     }
